3b6b35852768b3a0d7d8cd6737ab4335fde6997c
caroltreacy <caroltreacy85@gmail.com>
2020-03-23 20:36:44 +0000

    work today

--- squareroot.py
+++ squareroot.py
@@ -1,13 +1,17 @@
-# write a programe that takes a positive floating point number as input and 
+# Carol Treacy
+# write a program that takes a positive floating point number as input and 
 # outputs an approximation of its square
 
-# ptyhon squareroot.py
+# python squareroot.py
 # please enter a postive number 14.5
 # The square root of 14.5 is approx 3.8
 
-x = float(input("positive number to be square rooted"))
+positive number = float(input("positive number to be square rooted"))
+# postive number my program will request 
 
-y = def f(2) :
-    return y * X
+y = def f(X) :
+    return X * X
+    (f(2))
+# the value of 'y' is the squareroot of 'x'
 
-print('The square root of 'x' is approximately 'y')
+print('The square root of' positive number 'is approximately' y)
