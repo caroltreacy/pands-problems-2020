bdca0c0cfac0ca2f7c0d2a9d933cef652217429e
caroltreacy <caroltreacy85@gmail.com>
2020-03-29 15:13:28 +0100

    started program

---
+++ plots.py
@@ -0,0 +1,8 @@
+# Carol Treacy
+# write a programe that displays a plot of the functions
+#f(X) = x, g(X)=X*2, h(X)=x*3 in the rnage [0,4]
+#on the one set of axes 
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
